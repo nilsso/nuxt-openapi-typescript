954c0b6ca807bdbe6fc5229746f4342f22649e3a
Nils Olsson <nilso@enosis.net>
2023-02-26 10:55:15 -0800

    Lazy commit

--- dist/runtime/composables/useOpenAPI.mjs
+++ dist/runtime/composables/useOpenAPI.mjs
@@ -1,24 +1,22 @@
 import { Fetcher } from "openapi-typescript-fetch";
 import { useRuntimeConfig } from "#imports";
 export const useOpenAPI = () => {
-  return () => {
-    const { openapiTS } = useRuntimeConfig();
-    const baseUrl = openapiTS.apiUrl;
-    const fetcher = Fetcher.for();
-    fetcher.configure({
-      baseUrl,
-      init: {
-        credentials: "omit",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        }
+  const { openapiTS } = useRuntimeConfig();
+  const baseUrl = openapiTS.apiUrl;
+  const fetcher = Fetcher.for();
+  fetcher.configure({
+    baseUrl,
+    init: {
+      credentials: "omit",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
       }
-    });
-    const createApiFetch = (p, m) => {
-      const query = fetcher.path(p).method(m).create();
-      return (p2) => query(p2).then(({ data }) => data);
-    };
-    return createApiFetch;
+    }
+  });
+  const createApiFetch = (p, m) => {
+    const query = fetcher.path(p).method(m).create();
+    return (p2) => query(p2).then(({ data }) => data);
   };
+  return createApiFetch;
 };
--- src/runtime/composables/useOpenAPI.ts
+++ src/runtime/composables/useOpenAPI.ts
@@ -3,32 +3,30 @@ import { PathsObject } from 'openapi-typescript'
 // import { paths } from '#build/api'
 import { useRuntimeConfig } from '#imports'
 
-export const useOpenAPI = () => {
-  return <Paths extends PathsObject>() => {
-    const { openapiTS } = useRuntimeConfig()
-    const baseUrl = openapiTS.apiUrl
-    const fetcher = Fetcher.for<Paths>()
+export const useOpenAPI = <Paths extends PathsObject>() => {
+  const { openapiTS } = useRuntimeConfig()
+  const baseUrl = openapiTS.apiUrl
+  const fetcher = Fetcher.for<Paths>()
 
-    fetcher.configure({
-      baseUrl: baseUrl,
-      init: {
-        credentials: 'omit',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json'
-        }
+  fetcher.configure({
+    baseUrl: baseUrl,
+    init: {
+      credentials: 'omit',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
       }
-    })
-
-    const createApiFetch = <
-      P extends keyof Paths,
-      M extends keyof Paths[P],
-      Params extends OpArgType<Paths[P][M]>
-    >(p: P, m: M) => {
-      const query = fetcher.path(p).method(m).create()
-      return (p: Params) => query(p).then(({ data }) => data)
     }
+  })
 
-    return createApiFetch
+  const createApiFetch = <
+    P extends keyof Paths,
+    M extends keyof Paths[P],
+    Params extends OpArgType<Paths[P][M]>
+  >(p: P, m: M) => {
+    const query = fetcher.path(p).method(m).create()
+    return (p: Params) => query(p).then(({ data }) => data)
   }
+
+  return createApiFetch
 }
